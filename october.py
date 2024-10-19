428654283b2272bcdb2891750453aef722889cf5
SebinPrince <sebinpaikada1@gmail.com>
2024-10-19 14:41:39 +0530

    Add files via upload

---
+++ october.py
@@ -0,0 +1,16 @@
+'''AUTHOR:SEBIN PRINCE
+DATE:19/10/24
+
+num_1=int(input("enter first num:"))
+num_2=int(input("enter second num:" ))
+num_3=int(input("enter third num:"))
+print("sum of num1 and num2 is:",num_1+num_2)
+print("subtract num2 from num2is:",num_2-num_1)
+print("The product of num1 and num2 is:",num_1*num_2)
+print("The quotient when num1 is divided by num2 is:",num_1/num_2)
+print("The remainder when num1 is divided by num2 is:",num_1%num_2)
+print("he result of (num1 + num2) * num3 / 2 is: result:",(num_1 + num_2) * num_3 / 2)
+'''
+
+
+
