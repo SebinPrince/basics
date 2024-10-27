40efd9fd0d19124e8d5ed124fad8b33794809201
SebinPrince <sebinpaikada1@gmail.com>
2024-10-27 23:09:10 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,6 +1,6 @@
 '''AUTHOR:SEBIN PRINCE
 DATE:25/10/24
-DESCRIPTION:AIM: Write a Python program to find the largest of three numbers. The program should take three numbers as input from the user and determine which one is the largest. Use conditional statements to compare the numbers.
+DESCRIPTION:AIM: Write a Python program to find the largest of two numbers. The program should take three numbers as input from the user and determine which one is the largest. Use conditional statements to compare the numbers.
 number1=int(input("enter first number:"))
 number2=int(input("enter second number:"))
 
