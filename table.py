fe89586a1172fc82e587e0ae8a0d36b006a22e32
SebinPrince <sebinpaikada1@gmail.com>
2024-11-18 23:38:34 +0530

    Create table.py

---
+++ table.py
@@ -0,0 +1,9 @@
+'''AUTHOR=SEBIN PRINCE
+DESCRIPTION=Print a python program of multiplication table
+a=int(input("enter a number:"))
+b=int(input("enter the limit"))
+c=1
+for c in range(1,b+1):
+    print(c,"*",a,"=",c*a)
+    c=c+1
+'''
