05873e6888deb0c31685c8344597d15ec366a902
SebinPrince <sebinpaikada1@gmail.com>
2024-11-28 22:19:04 +0530

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,8 @@
+'''AUTHOR:SEBIN PRINCE
+DESCRIPTION:Write a function called celcius to fahrenheit that makes a temperature in celcius and convert it to fahrenheit using th formula f=(c*9/5)+32
+'''
+def c_to_f():
+    a=int(input("Enter temp:"))
+    f=(a*9/5)+32
+    print(f)
+c_to_f()
