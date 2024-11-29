dd2a150f8d7de1ebd6d7cc9bd4be92d823dacc35
SebinPrince <sebinpaikada1@gmail.com>
2024-11-29 20:07:52 +0530

    Update right_triangle.py

--- right_triangle.py
+++ right_triangle.py
@@ -1,13 +1,14 @@
 '''AUTHOR:SEBIN PRINCE
 DESCRIPTION:A program that accepts the lengths of three sides of a triangle as inputs. The program should output whether or not the triangle is a right triangle (Recall from the Pythagorean Theorem that in a right triangle, the square of one side equals the sum of the squares of the other two sides). Implement using functions.
 '''
-def right_triangle():
-    a=int(input("Enter hypotenuse side:"))
-    b=int(input("enter second side:"))
-    c=int(input("enter third side:"))
-
-    if a*a==b*b+c*c:
-        print("right triangle")
+def triangle(a, b, c):
+    sides = [a, b, c]
+    sides.sort()
+    if sides[0] + sides[1] > sides[2]:
+        print("Right triangle")
     else:
-        print("not right triangle")
-right_triangle()
+        print("Not a right triangle")
+side1 = int(input("Enter side1: "))
+side2 = int(input("Enter side2: "))
+side3 = int(input("Enter side3: "))
+triangle(side1, side2, side3)
