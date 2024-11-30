226a2295a90c8175ac1e85eab4a1b864a89e9cd1
SebinPrince <sebinpaikada1@gmail.com>
2024-11-30 15:54:52 +0530

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,8 @@
+'''AUTHOR:SEBIN PRINCE
+DESCRIPTION:Write a Python program to reverse a string.
+'''
+def reverse_list():
+    a=[1,2,3,4,"a","b","c","d"]
+    a.reverse()
+    print(a)
+reverse_list()
