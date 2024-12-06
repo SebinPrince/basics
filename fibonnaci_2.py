91c3f0238810fdd8c32d0149a25ac50d54877147
SebinPrince <sebinpaikada1@gmail.com>
2024-12-06 14:29:06 +0530

    Create fibonnaci_2.py

---
+++ fibonnaci_2.py
@@ -0,0 +1,5 @@
+'''AUTHOR:SEBIN PRINCE
+DESCRIPTION:Program to define a module to find Fibonacci Numbers and import the module to another program.
+'''
+import fibonacci
+print(fibonacci.fibonacci_number(10))
